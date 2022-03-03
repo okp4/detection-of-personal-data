a6d8b6b8ebafcd791a60388b46feec5857942ee6
NourKired <n.kired@okp4.com>
2022-03-03 16:27:06 +0100

    feat: project initialization

---
+++ README.md
@@ -0,0 +1,30 @@
+# Personal Data Detection
+
+![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white)
+
+
+## Foreword
+This repository contains a service to retrieve personal data such as first and last names, license plates, phone numbers, emails, postal addresses from DataFrames and return a dictionary of labels that are most often found on the dataframe. 
+The labels are: person, emails, address, phones and license_plate.
+
+## Technology
+
+### Nltk
+NLTK is a leading platform for building Python programs to work with human language data. It provides easy-to-use interfaces to over 50 corpora and lexical resources such as WordNet, along with a suite of text processing libraries for classification, tokenization, stemming, tagging, parsing, and semantic reasoning, wrappers for industrial-strength NLP libraries, and an active discussion forum.
+
+### RE (Regular Expression)
+
+A regular expression is a method used in programming for pattern matching. Regular expressions provide a flexible and concise means to match strings of text.
+
+## Organization of the repository
+
+The project is organized as follows:
+```text
+
+├── README.md
+└── src                   
+    └── pii.py
+
+    
+
+```
---
+++ src/pii.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import nltk
+import re
+from common_regex import CommonRegex
+
+cities = list(set(pd.read_csv("cities_.csv").dropna()['0'].values))
+
+
+def preprocess(sent):
+    sent = nltk.word_tokenize(sent)
+    sent = nltk.pos_tag(sent)
+    return sent
+
+
+grammar = """ 
+        LC: {<NNP>*<CD><NN|NNP|NNS|JJ|NNPS|,|.>{2}(<NN|NNP|NNS|NNPS|JJ|,|.>+<CD>?<NNP>*)*.*}
+"""
+
+
+def license_plate(text : str) -> list:
+    license_plate = []
+    for m in re.finditer(r"[A-Z]{2}[-][0-9]{3}[-][A-Z]{2}", text):
+        license_plate.append('%02d-%02d: %s' % (m.start(), m.end(), m.group(0)))
+    return license_plate
+
+
+def phone_number(text: str) -> list:
+    phone_number = []
+    for m in re.finditer(r"((((\+|00)(\d{2}|\d{3}))[-.\s]?\d[-.\s]?)|(?:\d{2}[-.\s]?))(\d{2}[-.\s]?){4}", text):
+        phone_number.append('%02d-%02d: %s' % (m.start(), m.end(), m.group(0)))
+    return phone_number
+
+
+def findPeople(s: str) -> list:
+    p1 = [x.group(0) for x in re.compile(r"(mr|dr|mrs|dr|jr|sir|Mlle|Mme|M|Mr|M|Dr|mme|Sir|Jr|Mrs|name is|(he|He) is|(she|She) is|(elle|Elle) est|(Il|il) est)(.|\s)([A-Z]\w+( [A-Z]\w+)+)").finditer(s)]
+    p2 = [y.group(0) for y in re.compile(r"( [A-Z]\w+)+? [A-Z]\w+ (is|est|a)").finditer(s)]
+    return p1 + p2
+
+
+def funct_(sent: str):
+    for w in sent:
+        try:
+            if w in cities:
+                return sent
+        except:
+            pass
+    return None
+
+
+def pii(txt : str) -> dict:
+    try:
+        text_labelised = {}
+        text_labelised_loc = []
+        persons = findPeople(txt)
+        parsed_text = CommonRegex(txt)
+        sent = preprocess(txt)
+        cp = nltk.RegexpParser(grammar)
+        cs = cp.parse(sent)
+        if len(license_plate(txt)) != 0:
+            text_labelised["license_plate"] = license_plate(txt)
+        phones = list(set(phone_number(txt) + parsed_text.phones + parsed_text.phones_with_exts))
+        if len(phones) != 0:
+            text_labelised["phones"] = phones
+        if len(parsed_text.emails) != 0:
+            text_labelised["emails"] = parsed_text.emails
+        if len(persons) != 0:
+            text_labelised["Person"] = persons
+        text_labelised_loc = [list(zip(*br.leaves()))[0] for br in cs.subtrees()][1:]
+        text_labelised_loc = [funct_(loc) for loc in text_labelised_loc if funct_(loc) is not None]
+        if len(text_labelised_loc) != 0:
+            text_labelised['Adresse'] = text_labelised_loc
+        return text_labelised
+    except:
+        pass
+
+
+def count_labels(df: pd.DataFrame) -> dict:
+    dict_label = {}
+    for col in df.columns:
+        labels = df[col].apply(pii)
+        try:
+            for lab in labels:
+                for key in lab.keys():
+                    if key in dict_label:
+                        dict_label[key] += 1
+                    else:
+                        dict_label[key] = 1
+        except:
+            pass
+    return dict(sorted(dict_label.items(), key=lambda item: -item[1]))
