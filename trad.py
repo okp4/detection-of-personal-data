107db41b71657ee82cc5fb408a9851fd038b595e
NourKired <n.kired@okp4.com>
2022-04-25 16:09:20 +0200

    feat: translation module

---
+++ trad.py
@@ -0,0 +1,33 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import MarianTokenizer, MarianMTModel
+
+import torch
+
+
+def detect_language(text):
+    tokenizer = AutoTokenizer.from_pretrained("papluca/xlm-roberta-base-language-detection")
+
+    model = AutoModelForSequenceClassification.from_pretrained("papluca/xlm-roberta-base-language-detection")
+
+    inputs = tokenizer(text, return_tensors="pt")
+
+    with torch.no_grad():
+        logits = model(**inputs).logits
+
+    predicted_class_id = logits.argmax().item()
+    return model.config.id2label[predicted_class_id]
+
+def translate(text):
+    print('ok')
+    trg="en"
+    src= detect_language(text)
+    print(src)
+    model_name = f"Helsinki-NLP/opus-mt-{src}-{trg}"
+    model = MarianMTModel.from_pretrained(model_name)
+    tokenizer = MarianTokenizer.from_pretrained(model_name)
+    sample_text = text
+    batch = tokenizer([sample_text], return_tensors="pt")
+
+    generated_ids = model.generate(**batch)
+    result =tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    return result
