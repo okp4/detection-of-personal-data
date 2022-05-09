0a8621e83506d277e4f1910df846c8ff23560d42
Nour Kired <n.kired@okp4.com>
2022-05-09 15:43:40 +0200

    refactor: add flake8 to poetry

--- src/translator.py
+++ src/translator.py
@@ -4,16 +4,26 @@ import time
 import torch
 
 
-trg = "en"
-src = "fr"
-model_dt = AutoModelForSequenceClassification.from_pretrained("papluca/xlm-roberta-base-language-detection")
-tokenizer_dt = AutoTokenizer.from_pretrained("papluca/xlm-roberta-base-language-detection")
-model_name = f"Helsinki-NLP/opus-mt-{src}-{trg}"
-model = MarianMTModel.from_pretrained(model_name)
-tokenizer = MarianTokenizer.from_pretrained(model_name)
+class model_translate:
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if model_translate.__instance is None :
+            model_translate.__instance = super(model_translate, cls).__new__(cls, *args, **kwargs)
+        return model_translate.__instance
+
+    def get_models(self, src="fr"):
+        trg = "en"
+        model_dt = AutoModelForSequenceClassification.from_pretrained("papluca/xlm-roberta-base-language-detection")
+        tokenizer_dt = AutoTokenizer.from_pretrained("papluca/xlm-roberta-base-language-detection")
+        model_name = f"Helsinki-NLP/opus-mt-{src}-{trg}"
+        model = MarianMTModel.from_pretrained(model_name)
+        tokenizer = MarianTokenizer.from_pretrained(model_name)
+        return tokenizer_dt, model_dt, tokenizer, model
 
 
 def detect_language(text):
+    tokenizer_dt, model_dt, tokenizer, model = model_translate().get_models()
     inputs = tokenizer_dt(text, return_tensors="pt")
     with torch.no_grad():
         logits = model_dt(**inputs).logits
@@ -23,6 +33,7 @@ def detect_language(text):
 
 
 def translate(text):
+    tokenizer_dt, model_dt, tokenizer, model = model_translate().get_models()
     time.sleep(1)
     # trg="en"
     src = detect_language(text)
