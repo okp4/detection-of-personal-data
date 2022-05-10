8a83856c85d89d716696abb38c07c5a59355d760
Laval <l.jacquin@okp4.com>
2022-05-10 09:27:47 +0200

    perf: splitted get_model method for model_translate class

--- src/pii_translator.py
+++ src/pii_translator.py
@@ -12,27 +12,29 @@ class model_translate:
             model_translate.__instance = super(model_translate, cls).__new__(cls, *args, **kwargs)
         return model_translate.__instance
 
-    def get_models(self, src="fr", trg="en"):
+    def get_model_lang_detect(self):
         model_lang_detect = AutoModelForSequenceClassification.from_pretrained("papluca/xlm-roberta-base-language-detection")
         tokenizer_lang_detect = AutoTokenizer.from_pretrained("papluca/xlm-roberta-base-language-detection")
+        return tokenizer_lang_detect, model_lang_detect
+
+    def get_model_trans(self, src="fr", trg="en"):
         model_name_trans = f"Helsinki-NLP/opus-mt-{src}-{trg}"
         model_trans = MarianMTModel.from_pretrained(model_name_trans)
         tokenizer_trans = MarianTokenizer.from_pretrained(model_name_trans)
-        return tokenizer_lang_detect, model_lang_detect, tokenizer_trans, model_trans
+        return tokenizer_trans, model_trans
 
 
 def detect_language(text):
-    tokenizer_lang_detect, model_lang_detect, tokenizer_trans, model_trans = model_translate().get_models()
+    tokenizer_lang_detect, model_lang_detect = model_translate().get_model_lang_detect()
     inputs = tokenizer_lang_detect(text, return_tensors="pt")
     with torch.no_grad():
         logits = model_lang_detect(**inputs).logits
-
     predicted_class_id = logits.argmax().item()
     return model_lang_detect.config.id2label[predicted_class_id]
 
 
 def translate(text):
-    tokenizer_lang_detect, model_lang_detect, tokenizer_trans, model_trans = model_translate().get_models()
+    tokenizer_trans, model_trans = model_translate().get_model_trans()
     time.sleep(1)
     src = detect_language(text)
     if src == "en":
