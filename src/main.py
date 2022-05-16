2bcc969658c5223d274ca7b2cd8877d51f3b5603
NourKired <n.kired@okp4.com>
2022-05-16 17:03:01 +0200

    style: make flake8 happy

--- src/main.py
+++ src/main.py
@@ -1,7 +1,24 @@
 import click
 from transformers import pipeline
 from functions import predict
+import pandas as pd
+import os
+from tqdm import tqdm
 import __init__ as init
+import json
+from nltk.tokenize import sent_tokenize
+
+
+def validate_dir_path(ctx, param, value):
+    if not os.path.isdir(value):
+        raise click.BadParameter("path must point to an existing directory")
+    return value
+
+
+def validate_file_path(ctx, param, value):
+    if not os.path.isfile(value):
+        raise click.BadParameter("path must point to an existing file")
+    return value
 
 
 @click.group
@@ -18,7 +35,23 @@ def version():
 
 @cli.command
 @click.option(
-    "-s", "--sentence", "sentence", type=str, required=True, help="sentence to process"
+    "-i",
+    "--input",
+    "input_file",
+    type=str,
+    callback=validate_file_path,
+    required=True,
+    help="path to text file",
+)
+@click.option(
+    "-o",
+    "--output",
+    "out_dir",
+    type=str,
+    callback=validate_dir_path,
+    default=".",
+    show_default=True,
+    help="output directory where json file will be written",
 )
 @click.option(
     "-tr",
@@ -27,16 +60,42 @@ def version():
     type=float,
     default=0.9,
     show_default=True,
-    help="the minimum probability of private data",
+    help="the minimum probability of private data for label : cookie, location, health, social security number,Tax file number, credit card",
 )
-def detect_labels(sentence, thresh):
+def pii_detect(
+    out_dir,
+    input_file,
+    thresh,
+) -> list:
+    """Represents cli 'pii_detect' command"""
+    # validate_args(sentence, thresh)
+    text = " ".join(list(pd.read_csv(input_file, delimiter=";")))
+    df = sent_tokenize(text)
     pipe = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
-    """Detect personal data"""
-    predict(
-        pipe,
-        sentence,
-        thresh,
+    detected_labels: list = [
+        predict(pipe, sent, thresh) for sent in tqdm(df, total=len(df))
+    ]
+    results = list(zip(*detected_labels))[0]
+    results = list(filter(None, results))
+    sentences = list(zip(*detected_labels))[1]
+    sentences = list(filter(None, sentences))
+    outputs_detected = {}
+    detected = 0
+    if results != []:
+        detected = sum(list(zip(*results))[0])
+        outputs_detected = {
+            sent: reslt for (sent, reslt) in zip(sentences, list(zip(*results))[1])
+        }
+    output = {
+        "FileName": os.path.basename(input_file),
+        "Personal information detected": True if detected > 0 else False,
+        "Results": outputs_detected,
+    }
+    output_path = os.path.join(
+        "./", out_dir, "sheet_" + os.path.basename(input_file) + "_result.json"
     )
+    with open(output_path, "w") as fp:
+        json.dump(output, fp)
 
 
 if __name__ == "__main__":
